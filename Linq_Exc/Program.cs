fe2341077f01b402471dba8c9b876346eb33c18c
Nourhan <nourhan.elmahdy.513@gmail.com>
2026-05-21 00:01:35 +0300

    edited

--- Linq_Exc/Program.cs
+++ Linq_Exc/Program.cs
@@ -189,7 +189,7 @@
             List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
             var result = numbers.Skip(3);
-
+            
             foreach (var r in result)
             {
                 Console.WriteLine(r);
